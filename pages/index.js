1992eb4f9338931e9bab78b7d32f79ea6e04e70b
kevynhb <kevynh36@gmail.com>
2024-11-27 22:08:49 -0400

    Criacao do index e packeage.json

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// index -> significa índice, como se fosse o título de um livro e o número da página.
+// No next cada pasta que tenha um arquivo index.js dentro dela, automaticamente vira uma rota pública
+// outros nomes também viram uma rota púlica, é igual com php onde o nome do arquivo vira a rota de acesso via URL
+
+// Home com H maiusculo é convenção do React para componentes
+function Home() {
+    return <h1>Teste Olá mundo</h1>
+}
+
+// Default para mostrar ao next.js que esse é o arquivo padrão para redenrizar a página, a partir dele pode-se chamar outros componentes pelo export
+export default Home;
+
