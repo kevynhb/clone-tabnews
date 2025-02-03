42ed5d99e9ec3b477673a65a5dfcf168fcf691f6
kevynhb <kevynh36@gmail.com>
2025-02-02 23:54:18 -0400

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  // response.status(200).send("alunos do curso.dev são pessoas acima da média");
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const version = await database.query("SHOW server_version");
+  const formatVersion = version.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections");
+  const formatMaxConnections = parseInt(maxConnections.rows[0].max_connections);
+
+  const usedConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';",
+  );
+  const formatUsedConnections = parseInt(usedConnections.rows[0].count);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  // "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version_v: formatVersion,
+    max_connections: formatMaxConnections,
+    used_connections: formatUsedConnections,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,36 @@
 test("Get to /api/vi/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status"); // fetch é um cliente http para acessar os endpoints, faz request e recebe response
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json(); // esta fazendo Parsing do corpo http em texto puro para json
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  // Desafio
+  expect(responseBody.version_v).toBeDefined();
+  expect(responseBody.version_v).toEqual("16.0");
+
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.max_connections).toBeGreaterThan(0);
+
+  expect(responseBody.used_connections).toBeDefined();
+  expect(responseBody.used_connections).toEqual(1);
+  expect(responseBody.used_connections).toBeLessThanOrEqual(
+    //used menor ou igual ao max connections
+    responseBody.max_connections,
+  );
+
+  console.log(responseBody.version_v);
+  console.log(responseBody.max_connections);
+  console.log(responseBody.used_connections);
 });
+
+// comando .only do jest prioriza rodar somente este teste, e skipped os outros tests
+// test.only("Teste de SQL Injection", async () => {
+//   const response = await fetch(
+//     // "http://localhost:3000/api/v1/status?databaseName=local_db",
+//     "http://localhost:3000/api/v1/status?databaseName='; select pg_sleep(5); --",
+//   );
+// });
